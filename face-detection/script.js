5ed2a04be27faf9418a290487c5a2df3197046e2
basv1996 <bas.vugts@gmail.com>
2022-04-20 15:26:40 +0200

    face detection with highest emotion value

--- face-detection/script.js
+++ face-detection/script.js
@@ -1,4 +1,7 @@
 const video = document.getElementById('video')
+const mainEl = document.getElementById('main')
+const emotionSpan = document.getElementById('emotion')
+
 
 
     Promise.all([
@@ -24,15 +27,36 @@ function startVideo() {
 //startVideo()
 video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
-    document.body.append(canvas)
+    main.append(canvas)
     const displaySize = { width: video.width, height: video.height }
     faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () => {
       const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
       const resizedDetections = faceapi.resizeResults(detections, displaySize)
-      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-      faceapi.draw.drawDetections(canvas, resizedDetections)
-      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-      faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    }, 100)
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height) // creer een neiuwe canavs elke keer
+      faceapi.draw.drawDetections(canvas, resizedDetections) //teken op het canvas het blauwe vierkant en detecteer een gezicht
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections) //de landmarks zijn de lijntjes op het gezicht
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections) //laat de emotie zien van het gezicht
+      //console.log(detections)
+      const expressions = detections[0]['expressions']
+    //   if(detections){
+    //   console.log(detections[0]['expressions'])
+    //   }
+
+        let max = 0;
+        let maxKey = "";
+
+        for(let express in expressions){
+        if(expressions[express]> max){
+            max = expressions[express];
+            maxKey= express
+            }
+        }
+        emotionSpan.innerHTML=maxKey
+        //console.log(maxKey)
+
+     // console.log(Object.entries(expressions))
+      // console.log(expressions)
+      
+    }, 4000)
   })
